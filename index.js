a801ff66918e33f5bfeacee51b824b0ddd32412e
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-02-09 20:10:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(helmet());
-const port = 3001;
+const port = process.env.PORT || 3001;
 let response;
 
 const createResponse = (code, data, log) => {
